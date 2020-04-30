f9aa8e8ca97fa0aa4d85cd8fa138a9daa0060d6f
noah <noahvito@tatari.tv>
2020-04-30 14:09:00 -0400

    annotations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id         :integer          not null, primary key
+#  nickname   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Game < ApplicationRecord
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id         :integer          not null, primary key
+#  nickname   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
