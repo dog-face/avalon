b9de15847061a9b6bcf090ff68769cde7ea9063b
noah <noahvito@tatari.tv>
2020-04-30 17:52:33 -0400

    remove nickname from game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,6 @@
 # Table name: games
 #
 #  id         :integer          not null, primary key
-#  nickname   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_012721) do
+ActiveRecord::Schema.define(version: 2020_04_30_215131) do
 
   create_table "games", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "nickname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
