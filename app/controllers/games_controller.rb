34bf31e90232f4b3b552e19686c9164c2ac63caa
noah <noahvito@tatari.tv>
2020-04-29 22:26:52 -0400

    working! with pry

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,27 @@
+
 class GamesController < ApplicationController
+  def index
+    binding.pry
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    @game.save
+    redirect_to @game
+  end
+
+
+  private
+  def game_params
+    params.require(:game).permit(:nickname)
+  end
 end
